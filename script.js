03d1321365e678776dd255679249ef0821e9bfab
tonykekw <nicolaeantonio.stan@studenti.unipd.it>
2023-10-05 22:10:45 +0200

    nav remake

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+
+
+$('#check').change(function() {
+  if ($('#check').is(":checked")) {
+    $('body').css('overflow','hidden');
+  } else {
+    $('body').css('overflow','auto');
+  }
+});
+
+
+
+
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
